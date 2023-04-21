241517b218b408ac27d6ce31258623dac11a8d76
yinwei <cba5530@gmail.com>
2023-04-21 13:48:30 +0800

    install react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,28 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Single from './pages/Single'
+import Write from './pages/Write'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>
+  },
+  {
+    path: '/login',
+    element: <Login/>
+  },
+  {
+    path: '/register',
+    element: <Register/>
+  }
+])
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router}/>
     </div>
   );
 }
