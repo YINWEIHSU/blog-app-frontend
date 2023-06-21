2fc99f93e9783a43de016538b2365f315289f37b
yinwei <cba5530@gmail.com>
2023-06-21 15:39:17 +0800

    seperate frontend

--- backend/routes/tag.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const tagController = require('../controllers/tag.controllers')
-
-router.get('/', tagController.getTags)
-
-module.exports = router
